a03948d2a88313169f5812b64a5a0381a9c4a499
SLJoke <74165680+SLJoke@users.noreply.github.com>
2022-03-08 02:39:34 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,8 @@ import axios from "axios"
 export function request(config) {
     const instance = axios.create({
         // baseURL: 'http://localhost:3000/',
-        baseURL: 'https://xx-music-api.vercel.app',
+//         baseURL: 'https://xx-music-api.vercel.app',
+        baseUrl: 'http://cloud-music.pl-fe.cn/',
         timeout: 5000
     })
 
